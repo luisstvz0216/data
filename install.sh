e843da8c19fd35cb81311873b873f644d67c26ab
luisstvz0216 <stvz2002stvz@gmail.com>
2025-03-02 11:27:16 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
    pip install requests tqdm aiohttp
 
    # Descargar el script desde GitHub
-   curl -L -o mi_script.py https://raw.githubusercontent.com/tu_usuario/tu_repositorio/main/mi_script.py
+   curl -L -o mi_script.py https://raw.githubusercontent.com/luisstvz0216/data/refs/heads/main/vpnv1.py
 
    # Dar permisos de ejecución al script
    chmod +x mi_script.py
