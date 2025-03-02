f7a1319fe0a9b37fa99331a92c03db92b0f31da0
luisstvz0216 <stvz2002stvz@gmail.com>
2025-03-02 11:55:35 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,24 +1,24 @@
 import os
-   import requests
+import requests
 
-   # URL del script en GitHub
-   script_url = "https://raw.githubusercontent.com/luisstvz0216/data/refs/heads/main/vpnv1.py"
+# URL del script en GitHub
+script_url = "https://raw.githubusercontent.com/luisstvz0216/data/refs/heads/main/vpnv1.py"
 
-   # Descargar el script
-   print("Descargando el script...")
-   response = requests.get(script_url)
-   if response.status_code == 200:
-       with open("mi_script.py", "wb") as file:
-           file.write(response.content)
-       print("Script descargado correctamente.")
-   else:
-       print(f"Error al descargar el script. Código de estado: {response.status_code}")
-       exit(1)
+# Descargar el script
+print("Descargando el script...")
+response = requests.get(script_url)
+if response.status_code == 200:
+    with open("mi_script.py", "wb") as file:
+        file.write(response.content)
+    print("Script descargado correctamente.")
+else:
+    print(f"Error al descargar el script. Código de estado: {response.status_code}")
+    exit(1)
 
-   # Instalar dependencias
-   print("Instalando dependencias...")
-   os.system("pip install requests")  # Agrega aquí las dependencias necesarias
+# Instalar dependencias
+print("Instalando dependencias...")
+os.system("pip install aiohttp tqdm")  # Agrega aquí las dependencias necesarias
 
-   # Ejecutar el script
-   print("Ejecutando el script...")
-   os.system("python mi_script.py")
+# Ejecutar el script
+print("Ejecutando el script...")
+os.system("python vpnv1.py")
